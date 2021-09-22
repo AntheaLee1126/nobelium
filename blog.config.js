519b911aab06c7fe534fa69aa9219232361acf69
Anthea <teanee.li@gmail.com>
2021-09-22 21:22:39 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,9 +1,9 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
-  link: 'https://nobelium.vercel.app',
-  description: 'This gonna be an awesome website.',
+  title: "Anthea's Note",
+  author: 'Anthea',
+  email: '',
+  link: 'https://',
+  description: '隨手記錄',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
